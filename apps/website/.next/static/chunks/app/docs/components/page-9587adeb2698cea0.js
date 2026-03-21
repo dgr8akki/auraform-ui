4ce72f532c3043ce81018182f25febded202dba0
Aakash Pahuja <pahujaaakash5@gmail.com>
2026-03-21 18:19:22 +0000

    Remove .next

--- apps/website/.next/static/chunks/app/docs/components/page-9587adeb2698cea0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[769],{5322:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9142,23))}},e=>{e.O(0,[142,952,580,358],()=>e(e.s=5322)),_N_E=e.O()}]);
